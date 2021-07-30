e12271ab24e11101c36719e62e3f80cce7719c6a
dochi486 <93mskim@gmail.com>
2021-07-30 20:47:32 +0900

    좀비랑 플레이어 이동, 내비메쉬 만들었따

---
+++ Assets/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public float speed = 3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+            move.z += 1;
+        if (Input.GetKey(KeyCode.S))
+            move.z -= 1;
+        if (Input.GetKey(KeyCode.A))
+            move.x -= 1;
+        if (Input.GetKey(KeyCode.D))
+            move.x += 1;
+        if (move != Vector3.zero)
+        {
+            move.Normalize();
+            transform.Translate(speed * move * Time.deltaTime, Space.World);
+            transform.forward = move; //이동하는 방향 바라보게 한다.
+        }
+
+    }
+}
