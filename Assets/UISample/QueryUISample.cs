7e0be05b088dfc060a07a0b086356ad0cc6fc95f
dochi486 <93mskim@gmail.com>
2021-07-30 20:08:49 +0900

    Initial commit

---
+++ Assets/UISample/QueryUISample.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UISample
+{
+    public class QueryUISample : MonoBehaviour
+    {
+        public static QueryUISample Instace;
+        GameObject buttonBase;
+        private void Awake()
+        {
+            Instace = this;
+            buttonBase = transform.Find("ButtonGroup/Button").gameObject;
+            buttonBase.SetActive(false);
+        }
+
+        List<GameObject> buttons = new List<GameObject>();
+        Action<string> fn;
+        internal void Show(string content, Action<string> _fn
+            , params string[] buttonTexts)
+        {
+            fn = _fn;
+            transform.Find("ContentText").GetComponent<Text>().text = content;
+
+            Destroybuttons();
+
+            foreach (var buttonText in buttonTexts)
+            {
+                GameObject newButton = Instantiate(buttonBase, buttonBase.transform.parent);
+                newButton.SetActive(true);
+                buttons.Add(newButton);
+                Button button = newButton.GetComponent<Button>();
+                button.onClick.AddListener(() => { OnClick(button); });
+                newButton.GetComponentInChildren<Text>().text = buttonText;
+            }
+        }
+        void OnClick(Button button)
+        {
+            var text = button.GetComponentInChildren<Text>().text;
+            fn(text);
+            gameObject.SetActive(false);
+            Destroybuttons();
+        }
+
+        private void Destroybuttons()
+        {
+            buttons.ForEach(x => Destroy(x));
+            buttons.Clear();
+        }
+    }
+}
