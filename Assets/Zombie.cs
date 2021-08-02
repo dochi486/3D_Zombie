466953d1e0f681e8400868220c810abf2aaf3771
dochi486 <93mskim@gmail.com>
2021-08-02 20:46:04 +0900

    좀비 피격될 때 피 파티클 생성

--- Assets/Zombie.cs
+++ Assets/Zombie.cs
@@ -10,6 +10,14 @@ public class Zombie : MonoBehaviour
     Animator animator;
     float originalSpeed;
 
+    public float bloodEffectYposition = 1.3f; //피 이펙트의 y포지션
+    public GameObject bloodParticle;
+    private void CreateBloodEffect()
+    {
+        var pos = transform.position;
+        pos.y = bloodEffectYposition;
+        Instantiate(bloodParticle, pos, Quaternion.identity);
+    }
     IEnumerator Start()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -33,6 +41,8 @@ public class Zombie : MonoBehaviour
         //0번 레이어의 0번 프레임부터 다시 시작한다는 의미.
         //애니메이션이 플레이 중이더라도 처음으로 돌아가서 다시 시작하게 된다. 
 
+        CreateBloodEffect();
+
         //총알을 맞았을 때 뒤로 밀려난다. 
         PushBackMove(toMoveDirection);
 
@@ -53,6 +63,8 @@ public class Zombie : MonoBehaviour
             //원래 인보크를 사용할 때는 Invoke("함수이름",딜레이시간);으로 써야한다. 
         }
     }
+
+
     public float moveBackDistance = 0.1f;
     public float moveBackNoise = 0.1f;
     private void PushBackMove(Vector3 toMoveDirection)
