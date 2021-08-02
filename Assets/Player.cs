62eb3748fe3a80fb13482c181b2741ab229ac365
dochi486 <93mskim@gmail.com>
2021-08-02 16:51:58 +0900

    플레이어가 마우스 포인터 방향을 바라보도록 수정

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -25,6 +25,31 @@ public class Player : MonoBehaviour
     {
         Vector3 move = Vector3.zero;
 
+        Move();
+        Fire();
+
+        animator.SetFloat("DirX", move.x);
+        animator.SetFloat("DirY", move.z);
+        animator.SetFloat("Speed", move.sqrMagnitude);
+        //애니메이터의 파라미터 Speed를 실제 이동하는 속도 move.sqrMagnitude로 설정한다.
+    }
+    Plane plane = new Plane(new Vector3(0, 1, 0), 0);
+    void LookAtMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out float enter))
+        {
+            Vector3 hitPoint = ray.GetPoint(enter);
+            Vector3 dir = hitPoint = transform.position;
+            dir.y = transform.position.y;
+            dir.Normalize();
+            transform.forward = dir;
+        }
+    }
+    private Vector3 Move()
+    {
+        Vector3 move = Vector3.zero;
+
         if (Input.GetKey(KeyCode.W))
             move.z += 1;
         if (Input.GetKey(KeyCode.S))
@@ -37,9 +62,12 @@ public class Player : MonoBehaviour
         {
             move.Normalize();
             transform.Translate(speed * move * Time.deltaTime, Space.World);
-            transform.forward = move; //이동하는 방향 바라보게 한다.
+            //transform.forward = move; //이동하는 방향 바라보게 한다.
         }
-
+        return move;
+    }
+    private void Fire()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -47,9 +75,5 @@ public class Player : MonoBehaviour
             animator.Play("Shoot");
             Instantiate(bullet, bulletPosition.position, bulletPosition.rotation);
         }
-        animator.SetFloat("DirX", move.x);
-        animator.SetFloat("DirY", move.z);
-        animator.SetFloat("Speed", move.sqrMagnitude);
-        //애니메이터의 파라미터 Speed를 실제 이동하는 속도 move.sqrMagnitude로 설정한다.
     }
 }
