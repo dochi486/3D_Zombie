504b6eab388171a885743b7df96a55d973980681
dochi486 <93mskim@gmail.com>
2021-08-02 22:03:37 +0900

    주석추가

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -37,11 +37,11 @@ public class Zombie : MonoBehaviour
     internal void TakeHit(int damage, Vector3 toMoveDirection)
     {
         hp -= damage;
-        animator.Play(Random.Range(0,2) == 0 ? "TakeHit1" : "TakeHit2", 0,0);
+        animator.Play(Random.Range(0, 2) == 0 ? "TakeHit1" : "TakeHit2", 0, 0);
         //0번 레이어의 0번 프레임부터 다시 시작한다는 의미.
         //애니메이션이 플레이 중이더라도 처음으로 돌아가서 다시 시작하게 된다. 
 
-        CreateBloodEffect();
+        CreateBloodEffect(); //피격되면 피 이펙트 생성
 
         //총알을 맞았을 때 뒤로 밀려난다. 
         PushBackMove(toMoveDirection);
@@ -53,17 +53,13 @@ public class Zombie : MonoBehaviour
         Invoke(nameof(SetTakeHitSpeed), TakeHitStopSpeedTime);
 
 
-
-
-
         if (hp <= 0)
         {
             GetComponent<Collider>().enabled = false;
-            Invoke(nameof(Die), 1) ; //nameof를 사용하면 함수 이름 바꾸는 리팩토링할 때 유용
+            Invoke(nameof(Die), 1); //nameof를 사용하면 함수 이름 바꾸는 리팩토링할 때 유용
             //원래 인보크를 사용할 때는 Invoke("함수이름",딜레이시간);으로 써야한다. 
         }
     }
-
 
     public float moveBackDistance = 0.1f;
     public float moveBackNoise = 0.1f;
