e701e254c10e8c65783a2dd0f0468e411e113b5d
dochi486 <93mskim@gmail.com>
2021-08-03 19:28:33 +0900

    인보크 일반 함수로 변경

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -1,7 +1,5 @@
-﻿using DG.Tweening;
-using System;
+﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
@@ -46,7 +44,6 @@ public class Zombie : MonoBehaviour
                 yield return null;
         }
 
-
         //while (hp > 0)
         //{
         //    if (target)
@@ -58,30 +55,42 @@ public class Zombie : MonoBehaviour
     internal void TakeHit(int damage, Vector3 toMoveDirection)
     {
         hp -= damage;
+
+        //총알을 맞았을 때 뒤로 밀려난다. 
+        PushBackMove(toMoveDirection);
+
+        CurrentFsm = TakeHitFSM;
+    }
+
+    IEnumerator TakeHitFSM()
+    {
         animator.Play(Random.Range(0, 2) == 0 ? "TakeHit1" : "TakeHit2", 0, 0);
         //0번 레이어의 0번 프레임부터 다시 시작한다는 의미.
         //애니메이션이 플레이 중이더라도 처음으로 돌아가서 다시 시작하게 된다. 
 
         CreateBloodEffect(); //피격되면 피 이펙트 생성
 
-        //총알을 맞았을 때 뒤로 밀려난다. 
-        PushBackMove(toMoveDirection);
-
 
         //그리고 이동 스피드가 잠시동안 0이 된다.
         agent.speed = 0;
-        CancelInvoke(nameof(SetTakeHitSpeed)); //이전에 실행하고 있던 invoke를 꺼줘서 좀비의 내비에이전트 속도가 잠시 0이 되도록한다. 
-        //이전에 실행한 SetTakeHitSpeed를 취소해줘야 스피드0으로 설정한 것이 제대로 적용된다?
+        //CancelInvoke(nameof(SetOriginalSpeed)); //이전에 실행하고 있던 invoke를 꺼줘서 좀비의 내비에이전트 속도가 잠시 0이 되도록한다. 
+        ////이전에 실행한 SetTakeHitSpeed를 취소해줘야 스피드0으로 설정한 것이 제대로 적용된다?
+        //Invoke(nameof(SetOriginalSpeed), TakeHitStopSpeedTime);
+        ////좀비 내비메쉬에이전트의 스피드를 원래 속도로 돌려준다.  
+        //코루틴 안에 있는 인보크는 가독성만 낮아지므로 코루틴으로 바꿔주는 게 좋다. 
 
-        Invoke(nameof(SetTakeHitSpeed), TakeHitStopSpeedTime);
-        //좀비 내비메쉬에이전트의 스피드를 원래 속도로 돌려준다. 
+        yield return new WaitForSeconds(TakeHitStopSpeedTime); //인보크 대신에 쉬는 코드를 먼저 넣고
+        SetOriginalSpeed(); //에이전트의 속도를 원래 속도로 돌려주는 함수 실행.
 
         if (hp <= 0)
         {
             GetComponent<Collider>().enabled = false;
-            Invoke(nameof(Die), 1); //nameof를 사용하면 함수 이름 바꾸는 리팩토링할 때 유용
+            yield return new WaitForSeconds(1);
+            Die(); 
+            //Invoke(nameof("Die"),1);//nameof를 사용하면 함수 이름 바꾸는 리팩토링할 때 유용
             //원래 인보크를 사용할 때는 Invoke("함수이름",딜레이시간);으로 써야한다. 
         }
+        CurrentFsm = ChaseFSM; //피격 당한 후에 바로 FSM으로 추격모드 설정
     }
 
     public float moveBackDistance = 0.1f;
@@ -95,7 +104,7 @@ public class Zombie : MonoBehaviour
         transform.Translate(toMoveDirection * moveBackDistance, Space.World);
     }
     public float TakeHitStopSpeedTime = 0.1f;
-    private void SetTakeHitSpeed()
+    private void SetOriginalSpeed()
     {
         agent.speed = originalSpeed;
     }
