df50555e3c4729b28762cb6e5ee1555a896ba4f9
dochi486 <93mskim@gmail.com>
2021-08-03 20:33:53 +0900

    좀비 공격 범위 제대로 설정, 좀비 내비메쉬 에이전트 앵귤러 스피드 값 최대로 지정

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -87,7 +87,7 @@ public class Zombie : MonoBehaviour
         if (hp <= 0)
         {
             GetComponent<Collider>().enabled = false;
-            yield return new WaitForSeconds(1);
+            yield return new WaitForSeconds(0.11f);
             Die();
             //Invoke(nameof("Die"),1);//nameof를 사용하면 함수 이름 바꾸는 리팩토링할 때 유용
             //원래 인보크를 사용할 때는 Invoke("함수이름",딜레이시간);으로 써야한다. 
