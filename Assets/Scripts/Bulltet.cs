8efc653fd0a18c3b47ef56ad045c9ab77a6b0e86
dochi486 <93mskim@gmail.com>
2021-08-08 16:34:12 +0900

    플레이어가 좀비에게 랜덤한 대미지 입히도록 랜덤레인지 추가

--- Assets/Scripts/Bulltet.cs
+++ Assets/Scripts/Bulltet.cs
@@ -7,6 +7,7 @@ public class Bulltet : MonoBehaviour
     public float speed = 20;
     public float destroyTime = 1f;
     public int power = 20;
+    public int randomRange = 4;
     private void Start()
     {
         Destroy(gameObject, destroyTime);
@@ -16,6 +17,7 @@ public class Bulltet : MonoBehaviour
     {
         transform.Translate(new Vector3(0,0, speed * Time.deltaTime), Space.Self);
         //포워드를 사용하려면 
+        //transform.Translate(transform.forward * speed * Time.deltaTime, Space.World);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -24,7 +26,7 @@ public class Bulltet : MonoBehaviour
         //GC발생하는 코드 CompareTag는 GC발생 안해서 CampareTag가 더 좋다
         {
             var zombie = other.GetComponent<Zombie>();
-            zombie.TakeHit(power, transform.forward);
+            zombie.TakeHit(power + Random.Range(-randomRange, randomRange), transform.forward);
             Destroy(gameObject);
         }
     }
