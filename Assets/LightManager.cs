e8e26f38f5a9fda10ae4bc72387e656d0eb557b1
dochi486 <93mskim@gmail.com>
2021-08-08 23:37:43 +0900

    컨텍스트 메뉴로 낮밤 바꾸는 로직 추가

--- Assets/LightManager.cs
+++ Assets/LightManager.cs
@@ -4,6 +4,37 @@ using UnityEngine;
 
 public class LightManager : MonoBehaviour
 {
+    //Ambient Color 환경광 영향을 받기 때문에 ~~ 
     public Color dayColor;
     public Color nightColor;
+
+
+    [ContextMenu("SetDayLight")]
+    void SetDayLight()
+    {
+        var allLight = FindObjectsOfType<Light>();
+        foreach (var item in allLight)
+        {
+            if (item.type == LightType.Directional)
+                item.enabled = true;
+            else
+                item.enabled = false;
+        }
+        RenderSettings.ambientLight = dayColor;
+    }
+
+    [ContextMenu("SetNightLight")]
+    void SetNightLight()
+    {
+        var allLight = FindObjectsOfType<Light>();
+        foreach (var item in allLight)
+        {
+            if (item.type == LightType.Directional)
+                item.enabled = false;
+            else
+                item.enabled = true;
+        }
+        RenderSettings.ambientLight = dayColor;
+    }
+
 }
