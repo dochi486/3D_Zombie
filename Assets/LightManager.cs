3aae72a781674c56c19456c65c9ced8c1a95fff4
dochi486 <93mskim@gmail.com>
2021-08-09 00:45:15 +0900

    키다운으로 라이트값 바꾸는 함수 추가

--- Assets/LightManager.cs
+++ Assets/LightManager.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class LightManager : MonoBehaviour
 {
@@ -37,4 +39,60 @@ public class LightManager : MonoBehaviour
         RenderSettings.ambientLight = dayColor;
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+            ChangeDayLight();
+
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+            ChangeNightLight();
+    }
+
+    Dictionary<Light, float> allLight;
+    public float changeDuration = 4;
+    private void ChangeNightLight()
+    {
+        if(allLight == null)
+        {
+            allLight = new Dictionary<Light, float>();
+            var _allLight = FindObjectsOfType<Light>();
+            foreach (var item in _allLight)
+            {
+                allLight[item] = item.intensity;
+            }
+        }
+
+        foreach (var item in allLight)
+        {
+            item.Key.enabled = true;
+            if (item.Key.type == LightType.Directional)
+                DOTween.To(() => 0f, (x) => item.Key.intensity = x, item.Value, changeDuration);
+            else
+                DOTween.To(() => item.Value, (x) => item.Key.intensity = x, 0, changeDuration);
+        }
+        DOTween.To(() => Camera.main.backgroundColor, (x) => Camera.main.backgroundColor = x, dayColor, changeDuration);
+    }
+
+    private void ChangeDayLight()
+    {
+        if (allLight == null)
+        {
+            allLight = new Dictionary<Light, float>();
+            var _allLight = FindObjectsOfType<Light>();
+            foreach (var item in _allLight)
+            {
+                allLight[item] = item.intensity;
+            }
+        }
+
+        foreach (var item in allLight)
+        {
+            item.Key.enabled = true;
+            if (item.Key.type != LightType.Directional)
+                DOTween.To(() => 0f, (x) => item.Key.intensity = x, item.Value, changeDuration);
+            else
+                DOTween.To(() => item.Value, (x) => item.Key.intensity = x, 0, changeDuration);
+        }
+        DOTween.To(() => Camera.main.backgroundColor, (x) => Camera.main.backgroundColor = x, nightColor, changeDuration);
+    }
 }
