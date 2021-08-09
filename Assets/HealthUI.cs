35d8c2925b82111caf14435ab3ed3a5812bea03f
dochi486 <93mskim@gmail.com>
2021-08-09 16:21:52 +0900

    HP UI 스크립트 작성 중
    수업 때 사용한 형식은 배열인데 나는 일단 리스트로 해봤따

--- Assets/HealthUI.cs
+++ Assets/HealthUI.cs
@@ -1,8 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthUI : SingletonMonoBehavior<HealthUI>
 {
+    public List<Image> images = new List<Image>();
+    public Image enable, current, disable;
 
+    TextMeshProUGUI valueText;
+
+    protected override void OnInit()
+    {
+        valueText = transform.Find("ValueText").GetComponent<TextMeshProUGUI>(); 
+        //OnInit은 유니티 기본 함수의 실행 순서와 관계 없이 항상 실행되므로 초기화할 때 OnInit에서 사용하는 것이 좋다.
+    }
+    
 }
