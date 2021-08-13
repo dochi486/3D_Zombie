3c54cce1a5791830184e96753322bb456e602520
dochi486 <93mskim@gmail.com>
2021-08-13 15:07:54 +0900

    수류탄 포물선 그리는 프로젝트 폴더 추가

---
+++ Assets/ProjectileShooting/Scripts/Cursor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Cursor : MonoBehaviour 
+{    
+	void Update () 
+	{
+        if (Time.timeScale == 0)
+            return;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, float.MaxValue, 1 << LayerMask.NameToLayer("Ground")))
+        {
+            transform.position = hit.point;
+        }
+
+        transform.rotation = Quaternion.identity;
+	}
+}
